d48fa3bd56317729b28c47e636c8b7ef4d368f7c
Peter <pmuniz03@gmail.com>
2018-09-13 09:08:21 -0500

    initial upload

---
+++ favorite-movies/CustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTableViewCell.swift
+//  favorite-movies
+//
+//  Created by Peter Muniz on 9/12/18.
+//  Copyright © 2018 Peter Muniz. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var movieImageView: UIImageView!
+    @IBOutlet var movieTitle: UILabel!
+    @IBOutlet var movieYear: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
